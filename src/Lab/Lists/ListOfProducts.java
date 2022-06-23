98aafe417384371d3ae3e7f802817838dc5e632f
YanaMarkova <yana_markova@yahoo.com>
2022-06-23 12:25:26 +0200

    initial commit

---
+++ src/Lab/Lists/ListOfProducts.java
@@ -0,0 +1,27 @@
+package src.Lab.Lists;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class ListOfProducts {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        List<String> productsList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String products = scanner.nextLine();
+            productsList.add(products);
+        }
+
+        Collections.sort(productsList);
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%d.%s%n", i + 1, productsList.get(i));
+        }
+        System.out.println();
+    }
+}
