4295194821bf2b27a5149f669376d805714b489e
YanaMarkova <yana_markova@yahoo.com>
2022-06-23 13:20:37 +0200

    initial commit

---
+++ src/Lab/Lists/RemoveNegativesAndReverse.java
@@ -0,0 +1,26 @@
+package src.Lab.Lists;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class RemoveNegativesAndReverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbersList = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+
+        numbersList.removeIf(e -> e < 0);
+
+        if (numbersList.isEmpty()){
+            System.out.println("empty");
+        } else {
+            Collections.reverse(numbersList);
+            for (int numbers : numbersList){
+                System.out.print(numbers + " ");
+            }
+        }
+    }
+}
